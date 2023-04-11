8fce0dd46d3fa88fd024ca000801c16c0434acbe
Max Nikitin <40297478+ITSalt@users.noreply.github.com>
2023-04-11 21:45:33 +0300

    Update function.js
    Test glide example

--- function.js
+++ function.js
@@ -1,20 +1,17 @@
-// The function here takes the parameters that you
-// have declared in the `glide.json` file, in the
-// same order.
-window.function = function (in_digit, in_wordFor1, in_wordFor2_4, in_wordFor5_14) {
+window.function = function (str, start, end) {
   // For each parameter, its `.value` contains
   // either its value in the type you've declared,
   // or it's `undefined`.  This is a good place to
   // extract the `.value`s and assign default
   // values.
-  
-  const digit = String(in_digit.value) ?? null;
-  const wordFor1 = String(in_wordFor1.value) ?? ""; 
-  const wordFor2_4 = String(in_wordFor2_4.value) ?? "";
-  const wordFor5_14 = String(in_wordFor5_14.value) ?? "";
-  
-  const d = digit.toString().slice(-2);
-  return (d[0] === '1' || d[1] >= '5' || d[1] === '0') ? wordFor5_14 :
-         (d[1] === '1' ? wordFor1 : (d[1] >= '2' && d[1] <= '4' ? wordFor2_4 : wordFor5_14));
+  str = str.value ?? "";
+  start = start.value ?? 0;
+  end = end.value;
 
+  // Your function should return the exact type
+  // you've declared for the `result` in
+  // `glide.json`, or `undefined` if there's an
+  // error or no result can be produced, because a
+  // required input is `undefined`, for example.
+  return str.substring(start, end);
 }
