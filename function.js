200051abea5f477e177c8cac7c1d22377203c87d
Max Nikitin <40297478+ITSalt@users.noreply.github.com>
2023-04-11 18:26:01 +0300

    Update function.js
    Fix syntaxis

--- function.js
+++ function.js
@@ -8,9 +8,16 @@ window.function = function (digit, wordFor1, wordFor2_4, wordFor5_14) {
   // extract the `.value`s and assign default
   // values.
 
-  return wordFor1;
+  digit = digit.value ?? null;
+  wordFor1 = worFor1.value ?? null; 
+  wordFor2_4 = wordFor2_4 ?? null;
+  wordFor5_14 = wordFor5_14 ?? null;
   
-  const d = digit.toString().slice(-2);
-  return d[0] === '1' || d[1] >= '5' || d[1] === '0' ? wordFor5_14 :
-         d[1] === '1' ? wordFor1 : d[1] >= '2' && d[1] <= '4' ? wordFor2_4 : wordFor5_14;
+  if (digit && wordFor1 && wordFor2_4&&wordFor5_14) {
+    const d = digit.toString().slice(-2);
+    return d[0] === '1' || d[1] >= '5' || d[1] === '0' ? wordFor5_14 :
+           d[1] === '1' ? wordFor1 : d[1] >= '2' && d[1] <= '4' ? wordFor2_4 : wordFor5_14;
+  }
+  else
+    return "All parameters must be fill";
 }
