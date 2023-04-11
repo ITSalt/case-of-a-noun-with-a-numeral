d7655552c9869c12737a56a1b5af75b177a3ded2
Max Nikitin <40297478+ITSalt@users.noreply.github.com>
2023-04-11 21:41:02 +0300

    Update function.js
    Refactor variable names

--- function.js
+++ function.js
@@ -1,22 +1,20 @@
 // The function here takes the parameters that you
 // have declared in the `glide.json` file, in the
 // same order.
-window.function = function (digit, wordFor1, wordFor2_4, wordFor5_14) {
+window.function = function (in_digit, in_wordFor1, in_wordFor2_4, in_wordFor5_14) {
   // For each parameter, its `.value` contains
   // either its value in the type you've declared,
   // or it's `undefined`.  This is a good place to
   // extract the `.value`s and assign default
   // values.
   
-  return wordFor1.value;
-  
-  digit = digit.value ?? null;
-  wordFor1 = wordFor1.value ?? null; 
-  wordFor2_4 = wordFor2_4.value ?? null;
-  wordFor5_14 = wordFor5_14.value ?? null;
+  const digit = String(in_digit.value) ?? null;
+  const wordFor1 = String(in_wordFor1.value) ?? ""; 
+  const wordFor2_4 = String(in_wordFor2_4.value) ?? "";
+  const wordFor5_14 = String(in_wordFor5_14.value) ?? "";
   
   const d = digit.toString().slice(-2);
-  return d[0] === '1' || d[1] >= '5' || d[1] === '0' ? wordFor5_14 :
-         d[1] === '1' ? wordFor1 : d[1] >= '2' && d[1] <= '4' ? wordFor2_4 : wordFor5_14;
+  return (d[0] === '1' || d[1] >= '5' || d[1] === '0') ? wordFor5_14 :
+         (d[1] === '1' ? wordFor1 : (d[1] >= '2' && d[1] <= '4' ? wordFor2_4 : wordFor5_14));
 
 }
